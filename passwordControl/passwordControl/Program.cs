a5ebf5e556995d46c431eb787b74726d77b76103
AbdullahBoz <abdullahboz@outlook.com>
2022-05-10 18:41:06 +0300

    Create Program.cs

---
+++ passwordControl/passwordControl/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passwordControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Burada 3 hakkımız olan basit bir şifre kontrol kodu yazacağız.
+            int i = 0, password = 1234, tryng = 3;
+            while (i==0)
+            {
+                Console.Write("Enter your password: ");  
+                int n = Convert.ToInt32(Console.ReadLine());
+                if (n == password)  // Girilen sayı şifre ile eşit ise döngüden çıkılsın diyoruz.
+                {
+                    Console.WriteLine("Password is correct!");
+                    break;
+                }
+                else
+                {
+                    tryng = tryng - 1; // her denemede şifre hakkımızdan 1 eksiltiyoruz.
+                    if (tryng == 0)     // Şifre hakkımız 0'a eşitlenirse şifre bloke oldu yazsını gösterip döngüden çıkılsın diyoruz.
+                    {
+                        Console.WriteLine("Password is blocked!!!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please try again!");  // şifre 0'a eşit değilse tekrar şifre istiyoruz.
+                    }
+                    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
